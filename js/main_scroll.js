235213fd1e91243dcaa94d38781b1d5414bfceab
Kim Jiae <95462028+gimziae@users.noreply.github.com>
2022-11-08 19:08:25 +0900

    메인 기본 레이아웃 수정

---
+++ js/main_scroll.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".swiper", {
+    direction: "vertical",
+    loop: false,
+    mousewheel: true,
+    speed: 500,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+});
