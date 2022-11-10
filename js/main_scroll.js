21432a93504893a3d1c293fc41beece36de161be
Kim Jiae <95462028+gimziae@users.noreply.github.com>
2022-11-10 19:15:38 +0900

    센터 메뉴 추가

--- js/main_scroll.js
+++
@@ -1,9 +0,0 @@
-const swiper = new Swiper(".swiper", {
-    direction: "vertical",
-    loop: false,
-    mousewheel: true,
-    speed: 500,
-    scrollbar: {
-        el: ".swiper-scrollbar",
-    },
-});
